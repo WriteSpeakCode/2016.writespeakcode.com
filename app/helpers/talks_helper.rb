bd5ea43fa8f09d2c5b02e0666d9e543680297885
karaAJC <KaraAJC@gmail.com>
2016-06-01 14:24:38 -0500

    added talk_date method to show date instead of wsc day

--- app/helpers/talks_helper.rb
+++ app/helpers/talks_helper.rb
@@ -1,2 +1,16 @@
 module TalksHelper
+  def talk_date(day)
+    case day
+      when "write"
+        return "June 15"
+      when "speak"
+        return "June 16"
+      when "code"
+        return "June 17"
+      when "career"
+        return "June 18"
+      else
+        return "TBD"
+    end
+  end
 end
