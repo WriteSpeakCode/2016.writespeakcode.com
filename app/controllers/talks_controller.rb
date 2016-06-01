95e231c1f4c6ebda4bc160b9b82643f9c25348ed
karaAJC <KaraAJC@gmail.com>
2016-06-01 13:27:24 -0500

    sectioning talks out

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -1,5 +1,9 @@
 class TalksController < ApplicationController
   def index
-    @talks = Talk.all
+    # @talks = Talk.all
+    @keynotes = Talk.keynotes
+    @both_talks = Talk.where(first_year_track: true).where(alumna_track: true).where(keynote: false)
+    @alumna_talks = Talk.where(alumna_track: true).where(keynote: false)
+    @first_year_talks = Talk.where(first_year_track: true).where(keynote: false)
   end
 end
