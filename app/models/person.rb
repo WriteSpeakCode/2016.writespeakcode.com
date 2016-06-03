fd5f696950e88925ee6615640b811da83ad243fd
karaAJC <KaraAJC@gmail.com>
2016-06-03 15:39:55 -0500

    cleaning people view to show keynote speakers and talks per person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,11 +1,19 @@
 class Person < ActiveRecord::Base
+  has_many :talk_speakers
+  has_many :talks, through: :talk_speakers
+
   validates :name, presence: true
   validates :email, presence: true, email: true
 
   scope :displayable, -> { where(display: true) }
   scope :speakers_and_mentors, -> { where(mentor: true).order(:name) }
 
+  def self.keynote_speakers
+    joins(:talk_speakers).joins(:talks).where("talks.keynote = true")
+  end
+
   def slug
     name.parameterize
   end
+
 end
